d8247be0244283e55bffdb64812a1e0fd6399984
Piotr <piotrzawada08@gmail.com>
2023-01-21 21:26:32 +0000

    Initial commit
    Language: Java 17
    Build tool: Gradle - Groovy
    Framework: Spring Boot 3.0.2
    Dependencies: Lombok, Spring Web, Spring Security, Spring Data JPA,
    H2 Database, Validation, Spring Boot Actuator
    
    Description: Local Barber Shop Online Booking System.

---
+++ src/main/java/com/piotrzawada/BarberShopBookingSystem/BarberShopBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.piotrzawada.BarberShopBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberShopBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberShopBookingSystemApplication.class, args);
+	}
+
+}
