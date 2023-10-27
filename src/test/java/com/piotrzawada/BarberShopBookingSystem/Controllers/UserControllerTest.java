6079a589e0536f5ed7098ef434c9b3702c004a35
Piotr <piotrzawada08@gmail.com>
2023-10-27 23:08:27 +0100

    test: add JUnit test for UserController

---
+++ src/test/java/com/piotrzawada/BarberShopBookingSystem/Controllers/UserControllerTest.java
@@ -0,0 +1,80 @@
+package com.piotrzawada.BarberShopBookingSystem.Controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.piotrzawada.BarberShopBookingSystem.Config.TestSecurityConfig;
+import com.piotrzawada.BarberShopBookingSystem.Entities.AppUser;
+import com.piotrzawada.BarberShopBookingSystem.Services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(controllers = UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+@Import(TestSecurityConfig.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private AppUser appUser;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @BeforeEach
+    public void init() {
+        appUser = AppUser.builder()
+                .nickname("Adam")
+                .email("adam443243433@gmail.com")
+                .role("ROLE_USER")
+                .password("Password123#~~")
+                .build();
+    }
+
+    @Test
+    void userController_register_returnOK() throws Exception {
+        given(userService.userExist(ArgumentMatchers.any())).willReturn(false);
+        given(userService.registerUser(ArgumentMatchers.any())).willReturn(appUser);
+
+        ResultActions resultActions = mockMvc.perform(post("/api/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(appUser)));
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("{\"message\":\"User has been registered\"}"));
+    }
+    @Test
+    void userController_register_returnConflict() throws Exception {
+
+        given(userService.userExist(ArgumentMatchers.any())).willReturn(true);
+        given(userService.registerUser(ArgumentMatchers.any())).willReturn(appUser);
+
+        ResultActions resultActions = mockMvc.perform(post("/api/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(appUser)));
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isConflict())
+                .andExpect(MockMvcResultMatchers.content().json("{\"message\":\"User with that username already exists.\"}"));
+    }
+}
