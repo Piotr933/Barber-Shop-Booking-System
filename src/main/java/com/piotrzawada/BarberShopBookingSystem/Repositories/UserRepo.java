17331456e90eef2b243fb258e8cfb481bcc46d0a
Piotr <piotrzawada08@gmail.com>
2023-11-13 18:51:54 +0000

    docs: Update Javadoc
    User Repo - added the description to the query's
    
    The Javadoc has ben regenerated

--- src/main/java/com/piotrzawada/BarberShopBookingSystem/Repositories/UserRepo.java
+++ src/main/java/com/piotrzawada/BarberShopBookingSystem/Repositories/UserRepo.java
@@ -12,7 +12,18 @@ import java.util.List;
  * @version 1.0.0
  */
 public interface UserRepo extends CrudRepository<AppUser, Long> {
+
+    /**
+     * Retrieves a AppUser associated with the given email
+     * @param email - String value represents the AppUser email address
+     * @return AppUser
+     */
     AppUser findByEmail(String email);
 
+    /**
+     * Retrieves List of App Users by given role (Admin,User);
+     * @param role
+     * @return
+     */
     List<AppUser> findAllByRole(String role);
 }
