4c6d87f6e824e2c3fb91dc3b71ea9faeb6fa0e99
Piotr <piotrzawada08@gmail.com>
2024-03-04 19:02:27 +0000

    feat: enhance web security
    Previously, any user could make requests on the app except those
    specified in exception matchers. This behavior has now been revised.
    All requests are now denied by default.
    
    To achieve this, I have revamped the authorization for all requests and
    meticulously reviewed and updated all request matchers. This ensures
    that specified users have access to the requests they require.

--- src/main/java/com/piotrzawada/BarberShopBookingSystem/Config/WebSecurityConfig.java
+++ src/main/java/com/piotrzawada/BarberShopBookingSystem/Config/WebSecurityConfig.java
@@ -50,20 +50,20 @@ public class WebSecurityConfig {
         return  httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/test/testForAll").permitAll();
-                    auth.requestMatchers("/test/testAdmin").hasAnyRole("ADMIN");
-                    auth.requestMatchers("/api/bookings/book").hasAnyRole("USER", "ADMIN");
-                    auth.requestMatchers("/api/bookings/cancel").hasAnyRole("USER", "ADMIN");
-                    auth.requestMatchers("/api/bookings/myBookings").hasRole("USER");
+                    auth.requestMatchers("/api/admin/register/*").permitAll();
                     auth.requestMatchers("/api/admin/add").hasRole("ADMIN");
                     auth.requestMatchers("/api/admin/usersBookings").hasAnyRole("ADMIN");
                     auth.requestMatchers("/api/admin/cancelBooking").hasAnyRole("ADMIN");
+                    auth.requestMatchers("/api/bookings/availableTimes").permitAll();
+                    auth.requestMatchers("/api/bookings/book").hasAnyRole("USER", "ADMIN");
+                    auth.requestMatchers("/api/bookings/cancel").hasAnyRole("USER", "ADMIN");
+                    auth.requestMatchers("/api/bookings/myBookings").hasRole("USER");
                     auth.requestMatchers("/api/services/all").permitAll();
                     auth.requestMatchers("/api/services/add").hasAnyRole("ADMIN");
                     auth.requestMatchers("/api/services/update").hasAnyRole("ADMIN");
                     auth.requestMatchers("/api/services/delete").hasAnyRole("ADMIN");
-                    auth.requestMatchers("/").permitAll();
-                    auth.anyRequest().permitAll();
+                    auth.requestMatchers("/api/*").permitAll();
+                    auth.anyRequest().denyAll();
                 })
                 .headers().frameOptions().disable()
                 .and()
